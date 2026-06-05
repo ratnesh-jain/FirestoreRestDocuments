5d28bf952c7a8cf1131aab8b0a00fd23b900276a
Ratnesh <ratneshjain1993@gmail.com>
2026-06-05 18:43:26 +0530

    Set default log level to debug and promote trace logs to debug

--- Sources/FirestoreRestDocuments/Internal/Logging.swift
+++ Sources/FirestoreRestDocuments/Internal/Logging.swift
@@ -1,5 +1,9 @@
 import Logging
 
 extension Logger {
-  static let firestoreRestDocuments = Logger(label: "com.firestorerestdocuments")
+  static let firestoreRestDocuments: Logger = {
+    var logger = Logger(label: "com.firestorerestdocuments")
+    logger.logLevel = .debug
+    return logger
+  }()
 }
